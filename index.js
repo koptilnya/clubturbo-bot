1f263ea5ace8e8427d9e01ed741194f1a15898d9
Nikita Kruglickiy <wraker6@gmail.com>
2023-11-29 17:27:54 +0300

    Update

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ const subscribers = [
 const bot = new TelegramBot(token, { polling: true });
 
 (async function loop() {
+    const { price, available } = await getProduct(productLink)
+
+    if (available) {
+        for (const chatId of subscribers) {
+            notify(chatId, price);
+        }
+    }
+
+    setTimeout(loop, 30000);
+})();
+
+bot.onText(/^\/me/, (msg, match) => {
+    bot.sendMessage(msg.chat.id, `Chat ID: ${msg.chat.id}`);
+});
+
+bot.onText(/^\/check/, async (msg, match) => {
+    const product = await getProduct(productLink);
+
+    bot.sendMessage(msg.chat.id, JSON.stringify(product));
+});
+
+bot.on('message', (msg) => {
+    bot.sendMessage(msg.chat.id, 'I\'m alive!');
+});
+
+async function getProduct(productLink) {
     const response = await fetch(productLink);
     const html = await response.text();
 
@@ -24,18 +50,12 @@ const bot = new TelegramBot(token, { polling: true });
     const styleAttr = currentEl.getAttribute('style');
     const available = styleAttr === undefined;
 
-    if (available) {
-        for (const chatId of subscribers) {
-            notify(chatId, price);
-        }
+    return {
+        link: productLink,
+        price,
+        available
     }
-
-    setTimeout(loop, 30000);
-})();
-
-bot.on('message', (msg) => {
-    bot.sendMessage(msg.chat.id, 'I\'m alive!');
-})
+}
 
 function notify(chatId, price) {
     return bot.sendMessage(chatId, `Товар в наличии!\n${price}\n${productLink}`);
